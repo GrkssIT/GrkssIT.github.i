6d28f11405b4876f7a3a4d94a36dddf04b3076fd
MRkruzey <vakulich.ru@Gmail.com>
2017-08-16 01:04:04 +0200

    Initial commit

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1502838195,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/Фон.png",
+		"images/Окно-sheet0.png",
+		"images/Окно2-sheet0.png",
+		"images/ВвестиВкурс-sheet0.png",
+		"images/Затемнение-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"xd_connection.js"
+	]
+}
